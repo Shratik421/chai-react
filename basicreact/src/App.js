3cf777edb1d75a6b52a506174b081e6b5c75a6c0
Shratik421 <Shratikpunde@gmail.com>
2024-01-11 10:42:09 +0530

    project setup

---
+++ basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Chai from './Chai.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h3> chai is importing </h3>
+      <Chai/>
+    </div>
+  );
+}
+
+export default App;
---
+++ vitereactproject/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1> Chai coming from chai.js</h1>
+        <Chai />
+      </div>
+    </>
+  );
+}
+
+export default App;
